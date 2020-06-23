921f9c02ddf38cd527789a493414ce075280521d
Red Williams <red.devcs@gmail.com>
2020-06-23 03:52:56 -0400

    Added config, node modules, and package

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	console.log(message.content);
+	if (message.content === '!ping') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }
 });
 
 client.login('NjkwMDE4Njc1MDI1MjQ4MzMx.XvBj-w.Fc4CqZez-xMRPrMy1i7z7aR_PTY');
